f089cc020ca56de0836758afe0adb272a9deb803
steve <steve@meadsteve.dev>
2021-04-02 16:22:32 +0200

    check words too

--- isseven/app_definition.py
+++ isseven/app_definition.py
@@ -4,9 +4,11 @@ from typing import Collection, Callable
 from fastapi import FastAPI
 from lagom import Container
 from lagom.integrations.fast_api import FastApiIntegration
-from pydantic import BaseModel
 from starlette.staticfiles import StaticFiles
 
+
+from .checkers import is_integer_seven, is_the_word_seven
+from .models import IsSevenResult
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -16,27 +18,11 @@ app = FastAPI(title="Isseven")
 container = Container()
 deps = FastApiIntegration(container)
 
-
-class IsSevenResult(BaseModel):
-    isseven: bool
-    explanation: str
-
-
 SevenChecker = Callable[[str], IsSevenResult]
 
-
-def is_integer_seven(possible_seven: str) -> IsSevenResult:
-    try:
-        if int(possible_seven) == 7:
-            return IsSevenResult(isseven=True, explanation="According to python this casts to 7")
-    except:
-        pass
-
-    return IsSevenResult(isseven=False, explanation="Not an integer seven")
-
-
 container[Collection[SevenChecker]] = [
-    is_integer_seven
+    is_integer_seven,
+    is_the_word_seven
 ]
 
 
---
+++ isseven/checkers.py
@@ -0,0 +1,27 @@
+from typing import Tuple, Collection
+
+from .models import IsSevenResult
+
+
+def is_integer_seven(possible_seven: str) -> IsSevenResult:
+    try:
+        if int(possible_seven) == 7:
+            return IsSevenResult(isseven=True, explanation="According to python this casts to 7")
+    except:
+        pass
+
+    return IsSevenResult(isseven=False, explanation="Not an integer seven")
+
+
+sevens: Collection[Tuple[str, str]] = [
+    ("seven", "That was seven in english"),
+    ("sju", "That was seven in swedish"),
+]
+
+
+def is_the_word_seven(possible_seven: str) -> IsSevenResult:
+    tidied_possible_seven = possible_seven.strip().lower()
+    for (actual_seven, description) in sevens:
+        if actual_seven == tidied_possible_seven:
+            return IsSevenResult(isseven=True, explanation=description)
+    return IsSevenResult(isseven=False, explanation="Not the word seven")
