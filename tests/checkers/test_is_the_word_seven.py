d75d13fe74994da1b05c5e40e3fb02bc3f5c6009
steve <steve@meadsteve.dev>
2021-04-02 20:20:53 +0200

    test the existing checkers

---
+++ tests/checkers/test_is_the_word_seven.py
@@ -0,0 +1,12 @@
+import pytest
+
+from isseven.checkers import is_the_word_seven
+
+
+def test_it_fails_for_the_number_seven():
+    assert is_the_word_seven("7").isseven is False
+
+
+@pytest.mark.parametrize("word", ["seven", "sju"])
+def test_is_passes_for_words_that_mean_seven(word):
+    assert is_the_word_seven(word).isseven
