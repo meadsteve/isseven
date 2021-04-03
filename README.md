b603d0e70c413bc503a2022c3ae4c0a1ffa95b8b
steve <steve@meadsteve.dev>
2021-04-03 14:29:36 +0200

    more readme stuff

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Isseven
 
-For too long we've struggled to know if something isseven. Now thanks
-to the power of this we can be sure if **it** isseven.
+For too long we've struggled to know if something isseven. Now thanks to the power of this we can be sure if **it** isseven.
 
 Here are some example usages:
 
-**TODO**
+```bash
+curl {{HOSTED_URL}}/is/6
+# {"isseven":false,"explanation":"We tried. This doesn't seem to be seven"}%   
+
+curl {{HOSTED_URL}}/is/seven
+# {"isseven":true,"explanation":"That was seven in english"}
+
+curl {{HOSTED_URL}}/is/qqqqqqq
+# {"isseven":true,"explanation":"It was q repeated 7 times"}
+```
--- isseven/app_definition.py
+++ isseven/app_definition.py
@@ -1,13 +1,15 @@
 import os
+import socket
 from functools import lru_cache
 from typing import Collection
 
 from fastapi import FastAPI
-from lagom import Container
+from lagom import Container, Singleton
 from lagom.integrations.fast_api import FastApiIntegration
 from markdown import markdown
+from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
-
+from starlette.templating import Jinja2Templates
 
 from .checkers import (
     is_integer_seven,
@@ -25,6 +27,10 @@ app = FastAPI(title="Isseven")
 container = Container()
 deps = FastApiIntegration(container)
 
+container[Jinja2Templates] = Singleton(
+    lambda: Jinja2Templates(directory=__location__ + "/../templates")
+)
+
 container[Collection[SevenChecker]] = CheckerCollection(  # type: ignore
     is_integer_seven,
     is_the_word_seven,
@@ -33,18 +39,26 @@ container[Collection[SevenChecker]] = CheckerCollection(  # type: ignore
 )
 
 with open(__location__ + "/../README.md") as readme:
-    content = "\n".join(readme.readlines())
-    homepage_html = markdown(content)
+    content = "\n".join(readme.readlines()).replace(
+        "{{HOSTED_URL}}", f"https://{socket.gethostname()}"
+    )
+    homepage_html = markdown(content, extensions=["fenced_code"])
 
 
 @app.get("/", include_in_schema=False)
-def root():
-    return homepage_html
+def root(request: Request, templates=deps.depends(Jinja2Templates)):
+    return templates.TemplateResponse(
+        "page.html",
+        {
+            "content": homepage_html,
+            "request": request,
+        },
+    )
 
 
 @app.get("/is/{possible_seven}", response_model=IsSevenResult)
 @lru_cache(maxsize=128)
-def check(possible_seven: str, checkers: Collection[SevenChecker] = deps.depends(Collection[SevenChecker])):  # type: ignore
+def check(possible_seven: str, checkers=deps.depends(Collection[SevenChecker])):  # type: ignore
     for checker in checkers:
         result = checker(possible_seven)
         if result.isseven:
