9c5ec9ee8c8939648d64327b35f386fc88065bfc
steve <steve@meadsteve.dev>
2021-04-27 20:26:56 +0200

    add hypothesis checks

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 from typing import Collection
 
 import pytest
+from hypothesis import strategies, given, example
 
 from isseven.app_definition import check, container
 from isseven.models import IsSevenResult, SevenChecker, CheckerCollection
@@ -21,3 +22,34 @@ def test_if_a_checker_returns_a_success_that_result_is_returned():
 @pytest.mark.parametrize("checker", container[Collection[SevenChecker]])  # type: ignore
 def test_all_the_checkers_in_the_container_behave_well_with_an_empty_string(checker):
     assert isinstance(checker(""), IsSevenResult)
+
+
+@given(strategies.text())
+@example("seven")
+@example("neves")
+@example("")
+def test_it_works_for_any_sensible_text(text):
+    result = check(text, container[Collection[SevenChecker]])  # type: ignore
+    assert isinstance(result, IsSevenResult)
+
+
+@given(strategies.integers())
+@example(-1)
+@example(7)
+@example(0)
+def test_it_works_for_integers(number):
+    result = check(str(number), container[Collection[SevenChecker]])  # type: ignore
+    assert isinstance(result, IsSevenResult)
+
+
+@given(strategies.decimals())
+@example(7.0)
+def test_it_works_for_decimals(number):
+    result = check(str(number), container[Collection[SevenChecker]])  # type: ignore
+    assert isinstance(result, IsSevenResult)
+
+
+@given(strategies.datetimes())
+def test_it_works_for_dates(datetime):
+    result = check(str(datetime), container[Collection[SevenChecker]])  # type: ignore
+    assert isinstance(result, IsSevenResult)
