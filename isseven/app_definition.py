1013650cd70c43180a6f699f7f7fb24920b619f1
steve <steve@meadsteve.dev>
2021-11-18 13:03:21 +0100

    maybe?

--- isseven/app_definition.py
+++ isseven/app_definition.py
@@ -30,6 +30,8 @@ container[Collection[SevenChecker]] = ALL_CHECKERS  # type: ignore
 
 homepage_html = get_homepage_html(__location__ + "/../")
 
+DEFAULT_CACHE_SECONDS = 3600  # 1 hour
+
 
 @app.get("/", include_in_schema=False)
 def root(request: Request, templates=deps.depends(Jinja2Templates)):
@@ -49,12 +51,25 @@ def root(request: Request, templates=deps.depends(Jinja2Templates)):
     name="isseven",
 )
 @lru_cache(maxsize=128)
-def check(possible_seven: str, checkers=deps.depends(Collection[SevenChecker])):  # type: ignore
+def check(possible_seven: str, checkers: Collection[SevenChecker] = deps.depends(Collection[SevenChecker])):  # type: ignore
+    shortest_cache = DEFAULT_CACHE_SECONDS
     for checker in checkers:
         result = checker(possible_seven)
         if result.isseven:
-            return JSONResponse(content=result.dict(), headers={})
-    return JSONResponse(content=nope("We tried. This doesn't seem to be seven").dict())
+            lifespan = result.valid_for_seconds or DEFAULT_CACHE_SECONDS
+            return _make_json_response(result, lifespan)
+        elif result.valid_for_seconds:
+            shortest_cache = min(shortest_cache, result.valid_for_seconds)
+    return _make_json_response(
+        nope("We tried. This doesn't seem to be seven"), shortest_cache
+    )
+
+
+def _make_json_response(result: IsSevenResult, cache_for_seconds: int):
+    return JSONResponse(
+        content=result.dict(),
+        headers={"Cache-Control": f"max-age={cache_for_seconds}"},
+    )
 
 
 # If no other route matches assume that it might be a static file
--- tests/test_app.py
+++ tests/test_app.py
@@ -21,6 +21,32 @@ def test_if_a_checker_returns_a_success_that_result_is_returned():
     assert json.loads(json_response.body) == success_result.dict()
 
 
+def test_by_default_results_are_valid_for_an_hour():
+    success_result = IsSevenResult(isseven=True, explanation="testing")
+    json_response = check("something", FunctionCollection(lambda _s: success_result))
+    assert json_response.headers["Cache-Control"] == "max-age=3600"
+
+
+def test_non_seven_results_are_cacheable_for_an_hour_by_default():
+    json_response = check("something", FunctionCollection())
+    assert json_response.headers["Cache-Control"] == "max-age=3600"
+
+
+def test_checkers_can_control_their_cache_validity():
+    success_result = IsSevenResult(
+        isseven=True, explanation="testing", valid_for_seconds=30
+    )
+    json_response = check("something", FunctionCollection(lambda _s: success_result))
+    assert json_response.headers["Cache-Control"] == "max-age=30"
+
+
+def test_when_everything_returns_false_the_shortest_cache_is_honoured():
+    success_result = IsSevenResult(isseven=False, explanation="1", valid_for_seconds=30)
+    success_result = IsSevenResult(isseven=False, explanation="2", valid_for_seconds=15)
+    json_response = check("something", FunctionCollection(lambda _s: success_result))
+    assert json_response.headers["Cache-Control"] == "max-age=15"
+
+
 @pytest.mark.parametrize("checker", container[Collection[SevenChecker]])  # type: ignore
 def test_all_the_checkers_in_the_container_behave_well_with_an_empty_string(checker):
     assert isinstance(checker(""), IsSevenResult)
