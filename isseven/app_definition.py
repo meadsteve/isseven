65554bce1186e28703a451970ec9d50a17735ccf
Steve B <steve@meadsteve.dev>
2022-03-22 15:27:10 +0100

    Fix error with valid_for_seconds not being set in response

--- isseven/app_definition.py
+++ isseven/app_definition.py
@@ -66,6 +66,8 @@ def check(possible_seven: str, checkers: Collection[SevenChecker] = deps.depends
 
 
 def _make_json_response(result: IsSevenResult, cache_for_seconds: int):
+    content = result.dict()
+    content['valid_for_seconds'] = cache_for_seconds
     return JSONResponse(
         content=result.dict(),
         headers={"Cache-Control": f"max-age={cache_for_seconds}"},
